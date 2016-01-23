fb4a66fddd14a700b43fb3f4960e9ed27e2f2f16
evanwchen <evanwchen@gmail.com>
2016-01-22 16:30:19 -0800

    Set up game environment and board and implement functions to update scoreboard

--- client/watchout.js
+++ client/watchout.js
@@ -1,1 +1,29 @@
 // start slingin' some d3 here.
+
+
+// setup the game environment
+
+var environment = {
+  height: 450,
+  width: 700,
+  numEnemies: 15,
+  padding: 20
+};
+
+var score = {
+  currentScore: 0,
+  highScore: 0
+};
+
+// setup the game board
+
+var gameBoard = d3.select('.board').append('svg:svg')
+  .attr('width', environment.width).attr('height', environment.height);
+
+
+// update current score and high score
+
+d3.select('.current').text(score.currentScore.toString());
+
+score.highScore = Math.max(score.currentScore, score.highScore);
+d3.select('.highscore').text(score.highScore);
